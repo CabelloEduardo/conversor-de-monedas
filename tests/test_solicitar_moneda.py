743ae801f5b23b95a6f8ec5efc81f4c205f5e945
Hector <hectoreduardo354@hotmail.com>
2025-05-11 19:37:23 -0600

    test: añade test para solicitar cantidad y mejora la validación de moneda

--- tests/test_solicitar_moneda.py
+++ tests/test_solicitar_moneda.py
@@ -5,7 +5,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from unittest.mock import patch
-from src.proyecto.metodos import solicitar_moneda, desea_continuar
+from src.proyecto.metodos import solicitar_moneda, solicitar_cantidad, desea_continuar
 
 def test_desea_continuar_si(monkeypatch):
     # Simulamos que el usuario ingresa 's'
@@ -16,6 +16,8 @@ def test_desea_continuar_no(monkeypatch):
     # Ahora simulamos que el usuario ingresa 'n'
     monkeypatch.setattr('builtins.input', lambda _: 'n')
     assert desea_continuar() == False
+
+### 
 
 def test_solicitar_moneda_valid_input():
     """Test that a valid currency is accepted."""
@@ -36,6 +38,14 @@ def test_solicitar_moneda_multiple_invalid_then_valid_input():
             assert solicitar_moneda("Introduce una moneda: ") == "MXN"
             assert mock_print.call_count == 2
             mock_print.assert_any_call("❌ Moneda no válida. Las opciones válidas son: ['USD', 'EUR', 'MXN']")
+
+###
+
+def test_solicitar_cantidad(monkeypatch):
+    with patch('builtins.input', side_effect=["abc", 12]):
+        with patch('builtins.print') as mock_print:
+            assert type(solicitar_cantidad()) == float
+            mock_print.assert_called_with("❌ Cantidad inválida. Por favor, introduce una cantidad válida.")
 
 ''' 
 def test_solicitar_moneda_valida_minuscula(monkeypatch):
